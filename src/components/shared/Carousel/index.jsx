958f1bed22ddf0f5d7235c48acc93457d4f5544e
Matt Moore <Matt.M.Moore13@outlook.com>
2021-12-28 14:34:55 -0500

    - Created a reuseable Carousel component

--- src/components/shared/Carousel/index.jsx
+++ src/components/shared/Carousel/index.jsx
@@ -1,7 +1,11 @@
-import React from 'react'
-import 'carousel.css'
+import React, { useState } from 'react'
+import Card from '../Card'
+import Paginator from '../Paginator'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faChevronLeft, faChevronRight } from "@fortawesome/free-solid-svg-icons"
+import './carousel.css'
 
-export default Carousel(props) {
+export default function Carousel(props) {
   const [index, setIndex] = useState(0)
   
   const slideLeft = () => {
@@ -11,7 +15,7 @@ export default Carousel(props) {
   }
   
   const slideRight = () => {
-    if (index + 1 <= services.length-1) {
+    if (index + 1 <= props.couches.length-1) {
       setIndex(index + 1)
     }
   }
@@ -53,7 +57,7 @@ export default Carousel(props) {
       if (offset <= -100) {
         slideRight();
         /* if we're at the last card, snap back to center */
-        if (index === services.length - 1) {
+        if (index === props.length - 1) {
           card.style.left = 0;
         } else {
           /* hide the shift back to center 
@@ -103,10 +107,25 @@ export default Carousel(props) {
   return (
     <div className='container'>
       <div className='card-container'>
-        {props.map((prop, n) => {
-          let position = n index ? "nextCard" : n === index ? "activeCard" : "prevCard"
-          return <Card {...prop} cardStyle={position}
+        {props.couches.map((couch, n) => {
+          let position = n > index ? "nextCard" : n === index ? "activeCard" : "prevCard"
+          return <Card {...couch} cardStyle={position} />
         })}
+        <FontAwesomeIcon
+          onClick={slideLeft}
+          className="leftBtn"
+          icon={faChevronLeft}
+        />
+        <FontAwesomeIcon
+          onClick={slideRight}
+          className="rightBtn"
+          icon={faChevronRight}
+        />
+        <Paginator
+          dataLength={props.length}
+          activeIndex={index}
+          handlePageChange={handlePageChange}
+        />
       </div>
     </div>
   )
